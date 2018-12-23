ef7b762f8c933c5d80b7c903dc4b6ea384b64bd9
as1n <novruzo.aslan.as@yandex.ru>
2018-12-23 22:34:58 +0300

    added spring security

--- chat/backend/src/main/java/com/novruzov/aslan/backend/service/impl/UserServiceImpl.java
+++ chat/backend/src/main/java/com/novruzov/aslan/backend/service/impl/UserServiceImpl.java
@@ -1,20 +1,48 @@
 package com.novruzov.aslan.backend.service.impl;
 
+import com.novruzov.aslan.backend.config.JwtTokenUtil;
 import com.novruzov.aslan.backend.entity.User;
 import com.novruzov.aslan.backend.repository.UserRepository;
 import com.novruzov.aslan.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Component
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserDetailsService, UserService {
     private UserRepository repository;
+
+    @Autowired
+    private BCryptPasswordEncoder bcryptEncoder;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
     public UserServiceImpl(UserRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = getUserByUsername(username).get();
+        if (user == null)
+            throw new UsernameNotFoundException("Invalid username or password.");
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthority(user));
+    }
+
+    private Set getAuthority(User user) {
+        Set authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().getName()));
+        return authorities;
     }
 
     @Override
